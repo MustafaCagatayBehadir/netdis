849d39a9bdd59c3e12580273604be4b2130963d3
mbehadir <mustafa.behadir@ses.com>
2024-06-18 22:03:05 +0000

    Huawei vsi tables are added.

--- discovery/vendors/huawei.py
+++ discovery/vendors/huawei.py
@@ -76,11 +76,75 @@ class HuaweiVrp(VendorBase):
         self.recreate_table(table_name, headers)
         self.insert_data_to_table(table_name, headers, parsed_output)
 
+    def mpls_ldp_remote_peer_info(self, _net_connect):
+        """
+        Retrieves the MPLS LDP (Label Distribution Protocol) remote peer information of the Huawei VRP device.
+
+        Args:
+            _net_connect: The Netmiko SSH connection object.
+
+        """
+        table_name = 'huawei_mpls_ldp_remote_peer'
+        output = _net_connect.send_command('display mpls ldp remote-peer')
+        template_path = self.discovery_templates_dir + '/huawei_vrp_display_mpls_ldp_remote-peer.textfsm'
+        headers, parsed_output = self.parse_output(output, template_path)
+        self.recreate_table(table_name, headers)
+        self.insert_data_to_table(table_name, headers, parsed_output)
+
+    def vsi_services_info(self, _net_connect):
+        """
+        Retrieves the VSI (Virtual Switch Instance) services information of the Huawei VRP device.
+
+        Args:
+            _net_connect: The Netmiko SSH connection object.
+
+        """
+        table_name = 'huawei_vsi_services'
+        output = _net_connect.send_command('display vsi services all')
+        template_path = self.discovery_templates_dir + '/huawei_vrp_display_vsi_services_all.textfsm'
+        headers, parsed_output = self.parse_output(output, template_path)
+        self.recreate_table(table_name, headers)
+        self.insert_data_to_table(table_name, headers, parsed_output)
+
+    def vsi_info(self, _net_connect):
+        """
+        Retrieves the VSI (Virtual Switch Instance) information of the Huawei VRP device.
+
+        Args:
+            _net_connect: The Netmiko SSH connection object.
+
+        """
+        table_name = 'huawei_vsi'
+        output = _net_connect.send_command('display vsi')
+        template_path = self.discovery_templates_dir + '/huawei_vrp_display_vsi.textfsm'
+        headers, parsed_output = self.parse_output(output, template_path)
+        self.recreate_table(table_name, headers)
+        self.insert_data_to_table(table_name, headers, parsed_output)
+
+    def vsi_peer_info(self, _net_connect):
+        """
+        Retrieves the VSI (Virtual Switch Instance) peer information of the Huawei VRP device.
+
+        Args:
+            _net_connect: The Netmiko SSH connection object.
+
+        """
+        table_name = 'huawei_vsi_peer'
+        output = _net_connect.send_command('display vsi peer-info')
+        template_path = self.discovery_templates_dir + '/huawei_vrp_display_vsi_peer-info.textfsm'
+        headers, parsed_output = self.parse_output(output, template_path)
+        self.recreate_table(table_name, headers)
+        self.insert_data_to_table(table_name, headers, parsed_output)
+
 
 if __name__ == '__main__':
     device = HuaweiVrp('huawei_vrp', 'clab-vm-vrp', 'admin', 'password')
-    with ConnectHandler(**device.device) as net_connect:
-        device.version_info(net_connect)
-        device.card_info(net_connect)
-        device.mda_info(net_connect)
-        device.transceiver_info(net_connect)
+    # with ConnectHandler(**device.device) as net_connect:
+    #     device.version_info(net_connect)
+    #     device.card_info(net_connect)
+    #     device.mda_info(net_connect)
+    #     device.transceiver_info(net_connect)
+    #     device.mpls_ldp_remote_peer_info(net_connect)
+    #     device.vsi_services_info(net_connect)
+    #     device.vsi_info(net_connect)
+    #     device.vsi_peer_info(net_connect)
--- discovery/vendors/nokia.py
+++ discovery/vendors/nokia.py
@@ -114,54 +114,7 @@ class NokiaSros(VendorBase):
             _net_connect: The Netmiko SSH connection object.
 
         """
-        # output = _net_connect.send_command('show service service-using vpls')
-        output = """
-===============================================================================
-Services [vpls]
-===============================================================================
-ServiceId    Type      Adm  Opr  CustomerId Service Name
--------------------------------------------------------------------------------
-110          VPLS      Up   Up   111000     ngn-rtp-zte-msan
-201          VPLS      Up   Up   1          vprn-111000|MEDIA-1|TG1_avea_lab
-210          VPLS      Up   Up   1          vprn-112000|SIG|TG1
-310          VPLS      Down Down 1          Huawei_SBC_Core_Sig
-333          VPLS      Up   Up   1          333
-373          VPLS      Up   Up   1          373
-555          VPLS      Down Down 1          555
-801          VPLS      Up   Up   1          vpls-801
-1000         VPLS      Up   Up   1          1000
-2400         VPLS      Up   Up   1          2400
-4091         VPLS      Up   Up   1          zte_dslam_mng
-10000        VPLS      Up   Down 1          ELAN001
-10001        VPLS      Up   Down 1          ELAN002
-10002        VPLS      Up   Down 1          ELAN003
-10003        VPLS      Up   Down 1          ELAN004
-10004        VPLS      Up   Down 1          ELAN005
-16011        VPLS      Down Down 1          16011
-17000        b-VPLS    Up   Down 1          17000
-18000        b-VPLS    Up   Up   1          18000
-20000        b-VPLS    Up   Up   1          20000
-22000        b-VPLS    Up   Up   1          22000
-25000        b-VPLS    Up   Up   1          25000
-55555        VPLS      Up   Up   1          evi-101
-69255        VPLS      Up   Up   1          69255
-88888        VPLS      Up   Down 1          Time_senk_Uydu_Ku
-324749       VPLS      Up   Up   1          324749
-324762       VPLS      Up   Up   1          324762
-644655       VPLS      Up   Up   1          644655
-650865       VPLS      Up   Up   1          650865
-652870       VPLS      Up   Up   652870     652870
-652871       VPLS      Up   Up   652871     652871
-653259       VPLS      Up   Up   1          zyntai|sync
-653442       VPLS      Up   Up   1          zyntai|mng
-653443       VPLS      Up   Up   1          653443
-6969690      VPLS      Up   Up   1          6969690
-333333333    VPLS      Up   Up   10         TTiM_to_istAvea|Single_MPLS_Test
--------------------------------------------------------------------------------
-Matching Services : 36
--------------------------------------------------------------------------------
-===============================================================================
-"""
+        output = _net_connect.send_command('show service service-using vpls')
         template_path = self.discovery_templates_dir + '/nokia_sros_show_service_service-using_vpls.textfsm'
         headers, parsed_output = self.parse_output(output, template_path)
         table_name = 'nokia_service_vpls'
@@ -199,13 +152,13 @@ Matching Services : 36
 
 if __name__ == '__main__':
     device = NokiaSros('nokia_sros', 'clab-vm-sros', 'admin', 'password')
-    # with ConnectHandler(**device.device) as net_connect:
-    #     device.version_info(net_connect)
-    #     device.chassis_info(net_connect)
-    #     device.card_info(net_connect)
-    #     device.mda_info(net_connect)
-    #     device.transceiver_info(net_connect)
-    #     device.service_sdp_info(net_connect)
-    #     device.service_sap_using_info(net_connect)
-    #     device.service_sdp_using_info(net_connect)
-    device.service_vpls_info(None)
+    with ConnectHandler(**device.device) as net_connect:
+        device.version_info(net_connect)
+        device.chassis_info(net_connect)
+        device.card_info(net_connect)
+        device.mda_info(net_connect)
+        device.transceiver_info(net_connect)
+        device.service_sdp_info(net_connect)
+        device.service_sap_using_info(net_connect)
+        device.service_sdp_using_info(net_connect)
+        device.service_vpls_info(net_connect)
